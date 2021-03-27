4a9eaa569e52a289943367930b47a77cbb1532de
ksom10 <someson11@aol.com>
2021-03-27 15:27:44 -0400

    index.php and User_controller.class
    index.php + user controller class

---
+++ controllers/user_controller.class.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Author: Kameron Someson
+ * Date: 3/26/2021
+ * File: user_controller.class.php
+ * Description:
+ */
+
+class UserController {
+    //Variables
+    private $index;
+    private $register;
+    private $login;
+    private $verify;
+    private $logout;
+    private $reset;
+    private $do_reset;
+    private $error;
+
+    public function __construct()
+    {
+        $this->index = new Index();
+        $this->register = new Register();
+        $this->login = new Login();
+        $this->verify = new Verify();
+        $this->logout = new Logout();
+        $this->reset = new Reset();
+        $this->do_reset = new confirmReset();
+        $this->error = new UserError();
+    }
+
+    //display the index page
+    public function displayIndex(){
+        $view = new Index();
+        $view->display();
+    }
+
+    //display the register page
+    public function registerUser(){
+        $view = new Register();
+        $view->display();
+    }
+
+    //display the login page
+    public function login(){
+        $view = new Login();
+        $view->display();
+    }
+
+    //display the verify page
+    public function verify(){
+        $view = new Verify();
+        $view->display();
+    }
+
+    //display the logout page
+    public function logout(){
+        $view = new Logout();
+        $view->display();
+    }
+
+    //display the reset page
+    public function reset(){
+        $view = new Reset();
+        $view->display();
+    }
+
+    //display the reset confirm page
+    public function doReset(){
+        $view = new confirmReset();
+        $view->display();
+    }
+
+    //display error
+    public function error($message){
+        //create an object of the Error class
+        $error = new UserError();
+
+        //display the error page
+        $error->display($message);
+    }
+}
--- index.php
+++ index.php
@@ -14,3 +14,41 @@ require_once ("vendor/autoload.php");
 $user_controller = new UserController();
 
 //add your code below this line to complete this file
+
+$action = "register";
+if (isset($_GET['action'])&& !(empty($_GET["action"])))
+    $action = $_GET['action'];
+
+//invoke proper method defined
+if ($action === 'index'){
+
+    $user_controller->displayIndex();
+
+} else if ($action === 'register'){
+    $user_controller->RegisterUser();
+
+} else if ($action === 'login'){
+    $user_controller->Login();
+
+} else if ($action === 'verify'){
+    $user_controller->verifyUser();
+
+} else if ($action === 'logout'){
+    $user_controller->Logout();
+
+}else if ($action === 'reset'){
+    $user_controller->Reset();
+
+}else if ($action === 'do_reset'){
+    $user_controller->doReset();
+
+} else if ($action === 'error'){
+    $message = "An error has occurred.";
+    if((isset($_GET['message'])) && !(empty($_GET['message'])))
+        $message = $_GET['message'];
+
+    $user_controller->error($message);
+} else {
+    $message = "Invalid action was requested.";
+    $user_controller->error($message);
+}
